92c651f7e0308b3ac3383a7170e876a975f9b0e4
jessplotkin <jepl4601@colorado.edu>
2024-11-04 14:10:50 -0700

    wa10

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+let storyText = "It was a bright and chilly day, so :insertx: decided to go for a hike. When they reached :inserty:, they were astonished to see :insertz:. Jess saw the whole thing and just laughed — :insertx: was always finding the wildest adventures.";
+let insertX = ["Jess the Brave", "JHP the Explorer", "Superwoman"];
+let insertY = ["The rockies", "the pearl street mall", "the hidden flatirons"];
+let insertZ = ["a treasure chest ", "a magical creature dancing", "A mountain lion"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:', xItem).replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300 * 0.0714286) + ' stone';
+    const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+    
+    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('94 fahrenheit', temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
